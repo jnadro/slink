b62cf6ac08ebd3afb91e04340e033b9bb37306fe
Jason Nadro <jason.nadro@gmail.com>
2014-01-21 22:00:18 -0600

    Added simple shader string literals
    -Don't want to deal with loading shader files from disk quite yet so
    let's just make them strings.

---
+++ src/slink/ShaderStrings.cpp
@@ -0,0 +1,23 @@
+#ifndef ShaderStrings_h
+#define ShaderStrings_h
+
+const char* VS = 
+	"struct VS_INPUT {"
+	"};"
+	"struct VS_OUTPUT {"
+	"};"
+	"VS_OUTPUT VS(in VS_INPUT input)"
+	"{"
+	"	VS_OUTPUT output = (VS_OUTPUT)0;"
+	"	return output;"
+	"}";
+
+const char* PS =
+	"struct VS_OUTPUT {"
+	"};"
+	"float4 PS(in VS_OUTPUT input) : SV_Target"
+	"{"
+	"	return float4(1.0f, 0.0, 1.0f, 1.0f);"
+	"}";
+
+#endif
