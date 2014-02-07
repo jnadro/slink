50a674031d76c7f68fe5793e85c14c0234d9b1a9
Jason Nadro <jason.nadro@gmail.com>
2014-02-06 21:21:40 -0600

    Send a 'drawcommand

--- src/slinkFXTool/js/main.js
+++ src/slinkFXTool/js/main.js
@@ -1,9 +1,16 @@
 var WebSocket = require('ws');
+
 var ws = new WebSocket('http://localhost:8080');
 console.log('ready...');
 ws.on('open', function() {
+	console.log('open');
 	ws.send('something');
 });
 ws.on('message', function(data, flags) {
 	console.log(data);
 });
+
+var b = document.getElementById('draw');
+b.onclick = function() {
+	ws.send("DrawCommand");
+};
